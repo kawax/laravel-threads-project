693e67d2b55f7bf49328d56587caff3c4dd68395
kawax <kawaxbiz@gmail.com>
2024-07-11 16:07:01 +0900

    Update ThreadsToken.php

--- app/Http/Middleware/ThreadsToken.php
+++ app/Http/Middleware/ThreadsToken.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Exception;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,14 +12,13 @@ class ThreadsToken
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     * @throws Exception
      */
     public function handle(Request $request, Closure $next): Response
     {
         if (session()->has('threads_token')) {
             return $next($request);
-        } else {
-            abort(Response::HTTP_FORBIDDEN);
         }
+
+        abort(Response::HTTP_FORBIDDEN);
     }
 }
