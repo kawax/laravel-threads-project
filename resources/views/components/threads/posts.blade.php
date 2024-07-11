1ee208980bdc65ea5d4b0c26cb1a12828f1b6655
kawax <kawaxbiz@gmail.com>
2024-07-11 15:40:30 +0900

    Update posts.blade.php

--- resources/views/components/threads/posts.blade.php
+++ resources/views/components/threads/posts.blade.php
@@ -2,11 +2,12 @@
     @forelse($posts as $post)
         <div class="border my-6">
             <ul>
-                @forelse($post as $key => $value)
+                @foreach($post as $key => $value)
                     <li><span class="font-bold mr-3">{{ $key }}</span><span>{{ $value }}</span></li>
-                @endforelse
+                @endforeach
             </ul>
         </div>
     @empty
+        <div>empty</div>
     @endforelse
 </div>
