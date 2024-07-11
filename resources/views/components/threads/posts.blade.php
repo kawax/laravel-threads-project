e8eed74530efc0c16b3a2fc0adabe7fa3ba49681
kawax <kawaxbiz@gmail.com>
2024-07-11 15:51:22 +0900

    Update posts.blade.php

--- resources/views/components/threads/posts.blade.php
+++ resources/views/components/threads/posts.blade.php
@@ -1,6 +1,6 @@
 <div>
     @forelse($posts as $post)
-        <table class="w-auto table-auto border-2 border-spacing-2 rounded my-6">
+        <table class="w-full table-auto border-2 border-spacing-2 rounded my-6">
             @foreach($post as $key => $value)
                 @if(is_string($value))
                     <tr>
