0974d5dab1d5c850ad61aa4267acdbd6f95ddda4
kawax <kawaxbiz@gmail.com>
2024-07-11 15:52:22 +0900

    Update posts.blade.php

--- resources/views/components/threads/posts.blade.php
+++ resources/views/components/threads/posts.blade.php
@@ -1,6 +1,6 @@
 <div>
     @forelse($posts as $post)
-        <table class="w-full table-auto border-2 border-spacing-2 rounded my-6">
+        <table class="table-auto my-6">
             @foreach($post as $key => $value)
                 @if(is_string($value))
                     <tr>
@@ -10,6 +10,7 @@
                 @endif
             @endforeach
         </table>
+        <hr>
     @empty
         <div>empty</div>
     @endforelse
