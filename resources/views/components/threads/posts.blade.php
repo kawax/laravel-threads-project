cb71b1d3527824fc1836f486ef53f86efe402b01
kawax <kawaxbiz@gmail.com>
2024-07-11 15:43:21 +0900

    Update posts.blade.php

--- resources/views/components/threads/posts.blade.php
+++ resources/views/components/threads/posts.blade.php
@@ -3,7 +3,9 @@
         <div class="border my-6">
             <ul>
                 @foreach($post as $key => $value)
-                    <li><span class="font-bold mr-3">{{ $key }}</span><span>{{ $value }}</span></li>
+                    @if(is_string($value))
+                        <li><span class="font-bold mr-3">{{ $key }}</span><span>{{ $value }}</span></li>
+                    @endif
                 @endforeach
             </ul>
         </div>
